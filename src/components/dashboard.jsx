adbce584c1ba1f4287c870e88c0b434f8264a855
Ben Rose <benrose207@gmail.com>
2020-11-11 12:17:02 -0800

    add date filters for charts

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Papa from 'papaparse';
+import { sortByDate, filterDataByDate } from '../utils/data_formatting_utils';
 import AvgTTS from './charts/avg_tts';
 import CustomersPerDay from './charts/customers_per_day';
 import CustomersPerDayPart from './charts/customers_per_dayPart';
@@ -7,7 +8,12 @@ import CustomersPerHour from './charts/customers_per_hour';
 import DailyCustomersTTS from './charts/daily_customers_tts';
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
+  const [initialData, setInitialData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [dateRange, setDateRange] = useState({
+    start: '',
+    end: ''
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,21 +23,52 @@ const Dashboard = () => {
       const decoder = new TextDecoder('utf-8');
       const csv = decoder.decode(result.value);
       const results = Papa.parse(csv, { header: true });
+      const sortedResults = sortByDate(results.data);
 
-      setData(results.data);
+      setInitialData(sortedResults);
     }
 
     fetchData();
   }, []);
 
+  const handleFilters = (e) => {
+    e.preventDefault();
+
+    if (dateRange.start || dateRange.end) {
+      const filteredByDate = filterDataByDate(initialData, dateRange.start, dateRange.end);
+      setFilteredData(filteredByDate);
+    } else {
+      setFilteredData(initialData);
+    }
+  };
+
+  const handleDateChange = (e) => {
+    setDateRange({ ...dateRange, [e.target.name]: e.target.value });
+  }
+
+  const handleFilterClear = () => {
+    setDateRange({ start: '', end: '' });
+    setFilteredData(initialData)
+  }
+
   return (
     <main>
-      <AvgTTS rawData={data} />
+      <div className="dashboard-header">
+        <AvgTTS rawData={filteredData.length ? filteredData : initialData} />
+        <form onSubmit={handleFilters}>
+          <label htmlFor="startDate">Start</label>
+          <input type="date" id="startDate" name="start" value={dateRange.start} onChange={handleDateChange}/>
+          <label htmlFor="endDate">End</label>
+          <input type="date" id="endDate" name="end" value={dateRange.end} onChange={handleDateChange} />
+          <button type="submit">Apply</button>
+          <button type="button" onClick={handleFilterClear} >Clear</button>
+        </form>
+      </div>
       <section className="charts-container">
-        <DailyCustomersTTS rawData={data}/>
-        <CustomersPerDay rawData={data} />
-        <CustomersPerDayPart rawData={data} />
-        <CustomersPerHour rawData={data} />
+        <DailyCustomersTTS rawData={filteredData.length ? filteredData : initialData}/>
+        <CustomersPerDay rawData={filteredData.length ? filteredData : initialData} />
+        <CustomersPerDayPart rawData={filteredData.length ? filteredData : initialData} />
+        <CustomersPerHour rawData={filteredData.length ? filteredData : initialData} />
       </section>
     </main>
   );
--- src/utils/data_formatting_utils.js
+++ src/utils/data_formatting_utils.js
@@ -6,10 +6,29 @@ export const getAvgTTS = (data) => {
 }
 
 export const getFormattedDate = (utcDate) => {
-  const date = new Date(parseInt(utcDate) * 1000);
+  const date = formatAsDate(utcDate);
   const month = date.getMonth();
   const day = date.getDate();
   const year = date.getFullYear();
 
   return `${month + 1}-${day}-${year}`;
+}
+
+export const sortByDate = (data) => {
+  const sorted = data.sort((a, b) => parseInt(a.first_seen_utc) - parseInt(b.first_seen_utc));
+  return sorted;
+}
+
+export const filterDataByDate = (data, startDate, endDate) => {
+  const start = startDate ? new Date(startDate) : formatAsDate(data[0].first_seen_utc);
+  const end = endDate ? new Date(endDate) : formatAsDate(data[data.length - 1].first_seen_utc);
+
+  return data.filter(customer => {
+    const customerDate = formatAsDate(customer.first_seen_utc)
+    return customerDate >= start && customerDate <= end;
+  });
+}
+
+const formatAsDate = (unixTime) => {
+  return new Date(parseInt(unixTime * 1000));
 }
